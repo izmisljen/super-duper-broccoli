420f00bf2676c79fb4d2de5ce2ff45792b285d8b
izmisljen <62943236+izmisljen@users.noreply.github.com>
2020-03-31 15:07:40 +0200

    Create nesto.cs

---
+++ nesto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_vj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            int var = 4;
+            var = var + 1;
+            Console.WriteLine("nova linija");
+        }
+    }
+}
